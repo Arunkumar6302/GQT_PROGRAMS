0b679d2488dabd82dd1b82ffa9a94102b3df1312
Arun2004 <rarunreddy05@gmail.com>
2026-02-05 08:34:24 +0530

    my first commit

---
+++ src/ArrayListDemo.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer> list = new ArrayList<>();
+		list.add(100);
+		list.add(125);
+		list.add(150);
+		list.add(75);
+		list.add(25);
+		list.add(20);
+		System.out.println(list);
+		
+
+	}
+
+}
